e17ffae3dc6283442ef15baf59f0c7de99ed76fe
Marigza <marigola@mail.ru>
2023-04-12 16:23:56 +0300

    feat: create simple-chaining function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -7,35 +7,30 @@ const { NotImplementedError } = require('../extensions/index.js');
 const chainMaker = {
   readyArr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    let arr = this.readyArr;
-    let num = arr.length;
-    return num;
+    this.length = this.readyArr.length
+    return this;
   },
   addLink(value) {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    let arr = this.readyArr;
-    arr.push(`( ${value} )`);
-    this.readyArr = arr;
+    this.readyArr = this.readyArr.concat(`( ${value} )`);
     return this;
   },
   removeLink(position) {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    if (Number.isInteger(position) && (position > 0 && position <= this.readyArr.length)) {
-      let arr = this.readyArr;
-      arr.splice(position - 1, 1);
-      this.readyArr = arr;
+    if (Number(position) && (position >= 1 && position <= this.readyArr.length)) {
+      this.readyArr.splice(position - 1, 1);
       return this;
     } else {
-      throw Error('You can\'t remove incorrect link!');
+      this.readyArr = [];
+      throw new Error('You can\'t remove incorrect link!');
     }
-    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
     let arr = this.readyArr;
     arr.reverse();
@@ -43,11 +38,11 @@ const chainMaker = {
     return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
-    let arr = this.readyArr;
-    this.readyArr = arr.join('~~');
-    return this;
+    let result = this.readyArr.join('~~');
+    this.readyArr = [];
+    return result;
   }
 };
 
