c5274cfb6a4ad4912c38cd5ce08210a16c0daab6
Marigza <marigola@mail.ru>
2023-04-14 15:13:14 +0300

    feat: create viginere-cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,89 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(str, key) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (str === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let strToUpperCase = str.toUpperCase();
+    let keyToUpperCase = key.toUpperCase();
+
+    let nounOfRepeatKey = Math.ceil(strToUpperCase.length / keyToUpperCase.length);
+    keyToUpperCase = keyToUpperCase.repeat(nounOfRepeatKey);
+
+    let keyA = 'A'.charCodeAt(0);
+    let abcCount = 26;
+
+    let cryptoStr = [];
+
+    for (let i = 0; i < strToUpperCase.length; i++) {
+      if (strToUpperCase.charCodeAt(i) < 65 || strToUpperCase.charCodeAt(i) > 90) {
+        cryptoStr.push(strToUpperCase[i]);
+        let keyToUpperCaseArr = keyToUpperCase.split('');
+        keyToUpperCaseArr.splice(i, 0, strToUpperCase[i]);
+        keyToUpperCase = keyToUpperCaseArr.join('');
+      } else {
+        let letterIdx = strToUpperCase.charCodeAt(i) - keyA;
+        let shift = keyToUpperCase.charCodeAt(i) - keyA;
+        cryptoStr.push(
+          String.fromCharCode(keyA + (letterIdx + shift) % abcCount)
+        );
+      }
+    }
+    if (this.type === false) {
+      cryptoStr.reverse();
+      return cryptoStr.join('');
+    } else {
+      return cryptoStr.join('');
+    }
+    
+  }
+  decrypt(str, key) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (str === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let strToUpperCase = str.toUpperCase();
+    let keyToUpperCase = key.toUpperCase();
+
+    let nounOfRepeatKey = Math.ceil(strToUpperCase.length / keyToUpperCase.length);
+    keyToUpperCase = keyToUpperCase.repeat(nounOfRepeatKey);
+
+    let keyA = 'A'.charCodeAt(0);
+    let abcCount = 26;
+
+    let DeCryptoStr = [];
+
+    for (let i = 0; i < strToUpperCase.length; i++) {
+      if (strToUpperCase.charCodeAt(i) < 65 || strToUpperCase.charCodeAt(i) > 90) {
+        DeCryptoStr.push(strToUpperCase[i]);
+        let keyToUpperCaseArr = keyToUpperCase.split('');
+        keyToUpperCaseArr.splice(i, 0, strToUpperCase[i])
+        keyToUpperCase = keyToUpperCaseArr.join('');
+      } else {
+        let letterIdx = strToUpperCase.charCodeAt(i) - keyA;
+        let shift = keyToUpperCase.charCodeAt(i) - keyA;
+        DeCryptoStr.push(
+          String.fromCharCode(keyA + (letterIdx - shift + abcCount) % abcCount)
+        );
+      }
+    }
+    if (this.type === false) {
+      DeCryptoStr.reverse();
+      return DeCryptoStr.join('');
+    } else {
+      return DeCryptoStr.join('');
+    }
   }
 }
 
